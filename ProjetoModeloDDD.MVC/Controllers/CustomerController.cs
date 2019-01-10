3a8e6259dc8eabed3b586148baf2b7c00c88737f
Rodrigo Brito <rodrigo@w3ti.com.br>
2019-01-10 16:48:40 -0200

    alterando chamado do repositorio direto para o service usando Injeção de Dependecia

--- ProjetoModeloDDD.MVC/Controllers/CustomerController.cs
+++ ProjetoModeloDDD.MVC/Controllers/CustomerController.cs
@@ -1,6 +1,6 @@
 ﻿using AutoMapper;
+using ProjetoModeloDDD.Application.Interfaces;
 using ProjetoModeloDDD.Domain.Entities;
-using ProjetoModeloDDD.Infra.Data.Repositories;
 using ProjetoModeloDDD.MVC.ViewModels;
 using System.Collections.Generic;
 using System.Web.Mvc;
@@ -9,12 +9,17 @@ namespace ProjetoModeloDDD.MVC.Controllers
 {
     public class CustomerController : Controller
     {
-        private readonly CustomerRepository customerRepository = new CustomerRepository();
+        private readonly ICustomerAppService _customerApp;
+
+        public CustomerController(ICustomerAppService customerApp)
+        {
+            _customerApp = customerApp;
+        }
 
         // GET: Customer
         public ActionResult Index()
         {
-            var customers = Mapper.Map<IEnumerable<Customer>, IEnumerable<CustomerViewModel>>(customerRepository.GetAll());
+            var customers = Mapper.Map<IEnumerable<Customer>, IEnumerable<CustomerViewModel>>(_customerApp.GetAll());
             return View(customers);
         }
 
